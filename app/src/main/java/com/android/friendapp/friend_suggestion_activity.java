118015d3dc1fc7f8fb78a1d708e1a80d382f2478
jaredscott <jmsmm92@gmail.com>
2015-07-23 15:42:07 -0500

    added a function to find who is 50% or greater in common with the person using the app.

--- app/src/main/java/com/android/friendapp/friend_suggestion_activity.java
+++ app/src/main/java/com/android/friendapp/friend_suggestion_activity.java
@@ -6,23 +6,103 @@ package com.android.friendapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 
 public class friend_suggestion_activity extends ActionBarActivity{
 
     private Integer images[] = {R.drawable.pic1, R.drawable.pic2, R.drawable.pic3,
     R.drawable.pic4, R.drawable.pic5};
+    private ParseUser user = ParseUser.getCurrentUser();
+    private List userList, commonList, comparison;
+
+    public void findUsersInCommon(){
+
+        Log.v("findUsersInCommon", "starting...");
+
+        commonList = new ArrayList<>();
+
+        final ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereExists("Interests");//.whereEqualTo("Interests", "female");
+        query.findInBackground(new FindCallback<ParseUser>() {
+            public void done(List<ParseUser> objects, ParseException e) {
+                if (e == null) {
+                    // The query was successful.
+                    Log.v("findUsersInCommon", "query successful...");
+
+                    userList = (List)user.get("Interests");
+                    double userSize = userList.size() - 1;
+                    Log.v("findUsersInCommon", "size of userSize is " + userSize + "and the size of the objects to compare is " + objects.size());
+
+                    //do this loop for each user in userList
+                    for(int i = objects.size() - 1; i > 0; --i){
+                        //start with the first item in objects and place it's list into comparison
+                        comparison = objects.get(i).getList("Interests");
+
+                        //this is what we use for iterations
+                        int iterations = (int) userSize;
+                        int percentage = (int) userSize;
+
+                        //see if each item in userList is in comparison
+                        Log.v("findUsersInCommon", "beginning first while loop...");
+
+                        while(iterations > 0){
+                            if( !comparison.contains(userList.get(iterations)))
+                                --percentage;
+
+                            --iterations;
+                        }
+
+                        Log.v("findUsersInCommon", "first while loop finished...");
+
+                        //if the percentage is >= 50%, add it to the common list
+                        if(percentage/userSize >= .5)
+                            commonList.add(comparison);
+                    }//end compairson for loop
+
+                    //now make sure that the list of objects in common is not over 5
+                    if(commonList.size() > 5){
+                        Log.v("findUsersInCommon", "commonList size is " + commonList.size());
+
+                        while(commonList.size() > 5){
+                            Random rand = new Random();
+                          //  int random = rand.nextInt((commonList.size() - 1) + 1);//it will never choose a number greater than commonLists' size
+                            commonList.remove(rand.nextInt((commonList.size() - 1) + 1));
+                        }
+                    }
+
+                } else {
+                    // Something went wrong.
+                }
+            }
+        });
+    }
+
+    //added by jared
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_suggestion);
         addImagesToThegallery();
+
+
+        //added by jared
+        findUsersInCommon();
     }
 
     private void addImagesToThegallery() {
